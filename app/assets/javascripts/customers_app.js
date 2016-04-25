75e229b6eb516b61cbe1ec448bebf60dfdc1d472
Tarah Nixon <erraticcreation@gmail.com>
2016-04-24 20:14:39 -0400

    Installed and configured Puma, also created some unit tests for Angular

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,23 +1,28 @@
-var app = angular.module('customers', [ ]);
+var app = angular.module('customers',[]);
 
 app.controller("CustomerSearchController", [
-          "$scope", "$http",
-  function($scope, $http) {
+          '$scope','$http',
+  function($scope , $http) {
 
     var page = 0;
 
     $scope.customers = [];
     $scope.search = function(searchTerm) {
+      $scope.loading = true;
       if (searchTerm.length < 3) {
         return;
       }
       $http.get("/customers.json",
                 { "params": { "keywords": searchTerm, "page": page } }
-      ).then(function(response) {
-        $scope.customers = response.data;
-      },function(response) {
-        alert("There was a problem: " + response.status);
-      });
+      ).success(
+        function(data,status,headers,config) {
+          $scope.customers = data;
+          $scope.loading = false;
+      }).error(
+        function(data,status,headers,config) {
+          $scope.loading = false;
+          alert("There was a problem: " + status);
+        });
     }
     $scope.previousPage = function() {
         if (page > 0) {
