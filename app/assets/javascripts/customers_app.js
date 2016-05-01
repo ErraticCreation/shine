687619027ff32bee161f8b393b66c64ab175e58e
Tarah Nixon <erraticcreation@gmail.com>
2016-04-30 20:54:25 -0400

    Decoupled the fake credit card processing functionality from the Angular customer details controller to using the Angular customer credit card controller

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -66,10 +66,10 @@ app.controller("CustomerDetailController", [
           "$scope", "$routeParams", "$resource",
   function($scope, $routeParams, $resource) {
 
-    var customerId = $routeParams.id;
+    $scope.customerId = $routeParams.id;
     var Customer = $resource('/customers/:customerId.json')
 
-    $scope.customer = Customer.get({ "customerId": customerId})
+    $scope.customer = Customer.get({ "customerId": $scope.customerId})
 
     $scope.customer.billingSameAsShipping = false;
     $scope.$watch('customer.billing_address_id',function() {
@@ -84,6 +84,10 @@ app.controller("CustomerCreditCardController", [
           "$scope", "$resource",
   function($scope, $resource) {
     var CreditCardInfo = $resource('fake_billing.json')
-    $scope.creditCard = CreditCardInfo.get({ "cardholder_id": 1234})
+    $scope.setCardholderId = function(cardholderId) {
+      $scope.creditCard = CreditCardInfo.get(
+        { "cardholder_id": cardholderId }
+      )
+    }
   }
 ]);
