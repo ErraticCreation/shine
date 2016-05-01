9b5ae58a3029284ed995733be22ba7ce3449c5bf
Tarah Nixon <erraticcreation@gmail.com>
2016-04-30 20:21:13 -0400

    Updated routes, created a fake credit card info controller to demonstrate billing info and updated the customer details page with a new Angular controller in order to display the relevent data

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -80,3 +80,11 @@ app.controller("CustomerDetailController", [
     });
   }
 ]);
+
+app.controller("CustomerCreditCardController", [
+          "$scope", "$resource",
+  function($scope, $resource) {
+    var CreditCardInfo = $resource('fake_billing.json')
+    $scope.creditCard = CreditCardInfo.get({ "cardholder_id": 1234})
+  }
+]);
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -25,7 +25,6 @@ class CustomersController < ApplicationController
 
   def show
     customer_detail = CustomerDetail.find(params[:id])
-    sleep 5
     respond_to do |format|
       format.json { render json: customer_detail }
     end
